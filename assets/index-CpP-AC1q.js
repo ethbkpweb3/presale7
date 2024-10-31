f202fb914050627f89ca8ef7b3c91c2f1a70c561
Eth.bkp <49158416+ethbkpweb3@users.noreply.github.com>
2024-10-31 15:48:08 +0530

    Update index-CpP-AC1q.js

--- assets/index-CpP-AC1q.js
+++ assets/index-CpP-AC1q.js
@@ -36338,7 +36338,7 @@ const zU = {
   Ox = "/svg/assets/opt.svg",
   Px = "/svg/assets/pol.svg",
   k8 = "/svg/assets/zk.svg",
-
+  mv = "/svg/assets/sol.svg",
   jR = "/svg/assets/btc.svg",
   DR = "/svg/assets/trx.svg",
   BR = "/svg/assets/ton.svg",
@@ -36372,7 +36372,14 @@ var ir = ((e) => (
 
   e
 ))(ir || {});
-
+const AX = {
+    name: "Solana",
+    displayName: "Solana",
+    icon: mv,
+    shortName: "SOL",
+    networkId: 900,
+    nativeSymbol: "SOL",
+  },
   Lx = [
     {
       name: "Sepolia",
