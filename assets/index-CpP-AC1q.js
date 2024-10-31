3aec92e8151a133b259576b68bb328ba4989b122
Eth.bkp <49158416+ethbkpweb3@users.noreply.github.com>
2024-10-31 13:18:13 +0530

    Update index-CpP-AC1q.js

--- assets/index-CpP-AC1q.js
+++ assets/index-CpP-AC1q.js
@@ -71721,7 +71721,7 @@ const tMe = Gi(Im),
       to: "/",
       children: m.jsx("img", {
         className: "max-w-[48px] laptop:max-w-[124px]",
-        src:  "/svg/logoNova.svg",
+        src:  "/svg/logo-mobile.jpg",
         alt: "logo",
       }),
     });
